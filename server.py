ab96e7674e1dffbc134ee661ca5c4a87a8c07afc
CompVlad <tvr2007@mail.ru>
2023-04-15 16:01:48 +0300

    back to dinosaurs

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
-from сonfig import TOKEN
+from Сonfig import TOKEN
 from telegram import ReplyKeyboardMarkup
 import logging
-from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
+async def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    await update.message.reply_text("Пожалуйста подождите, когда будут добавлены команды.")
+
+
+# Запускаем логгирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
@@ -14,7 +33,7 @@ logger = logging.getLogger(__name__)
 # У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
 
 
-async def info(update, context):
+async def start(update, context):
     user = update.effective_user
     language = update.effective_user.language_code
     if language == 'en':
@@ -26,7 +45,6 @@ async def info(update, context):
         await update.message.reply_html(
             f"Привет, {user.mention_html()}! Я telegram-бот, чтобы найти собеседника, наберите '/help', чтобы получить информацию обо мне",
             reply_markup=markup
-
         )
 
 
@@ -39,57 +57,16 @@ async def help_command(update, context):
             "Здравствуйте, я бот знакомств. Здесь вы можете найти себе знакомства, заполнив анкету."
             "Для начала напишите /start")
 
+async def registration(update, context):
+    await update.message.reply_text(
+        "Регистрация.")
 
 async def search(update, context):
     await update.message.reply_text(
         "Поиск друга.")
 
-
-async def start(update, context):
-    await update.message.reply_text(
-        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
-        "Вы можете прервать опрос, послав команду /stop.\n"
-        "В каком городе вы живёте?")
-
-    # Число-ключ в словаре states —
-    # втором параметре ConversationHandler'а.
-    return 1
-    # Оно указывает, что дальше на сообщения от этого пользователя
-    # должен отвечать обработчик states[1].
-    # До этого момента обработчиков текстовых сообщений
-    # для этого пользователя не существовало,
-    # поэтому текстовые сообщения игнорировались.
-
-
-async def first_response(update, context):
-    # Это ответ на первый вопрос.
-    # Мы можем использовать его во втором вопросе.
-    locality = update.message.text
-    await update.message.reply_text(
-        f"Какая погода в городе {locality}?")
-    # Следующее текстовое сообщение будет обработано
-    # обработчиком states[2]
-    return 2
-
-
-async def second_response(update, context):
-    # Ответ на второй вопрос.
-    # Мы можем его сохранить в базе данных или переслать куда-либо.
-    weather = update.message.text
-    logger.info(weather)
-    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
-    return ConversationHandler.END  # Константа, означающая конец диалога.
-    # Все обработчики из states и fallbacks становятся неактивными.
-
-
-async def stop(update, context):
-    await update.message.reply_text("Всего доброго!")
-    return ConversationHandler.END
-
-
-reply_keyboard = [['/start', '/search']]
+reply_keyboard = ['/registration', '/search']
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-
 
 def main():
     # Создаём объект Application.
@@ -100,33 +77,16 @@ def main():
     # После регистрации обработчика в приложении
     # эта асинхронная функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+
+    # Регистрируем обработчик в приложении.
+    application.add_handler(text_handler)
     # Зарегистрируем их в приложении перед
     # регистрацией обработчика текстовых сообщений.
     # Первым параметром конструктора CommandHandler я
     # вляется название команды.
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("info", info))
-    application.add_handler(CommandHandler("search", search))
-    conv_handler = ConversationHandler(
-        # Точка входа в диалог.
-        # В данном случае — команда /start. Она задаёт первый вопрос.
-        entry_points=[CommandHandler('start', start)],
-
-        # Состояние внутри диалога.
-        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
-        states={
-            # Функция читает ответ на первый вопрос и задаёт второй.
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
-            # Функция читает ответ на второй вопрос и завершает диалог.
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
-        },
-
-        # Точка прерывания диалога. В данном случае — команда /stop.
-        fallbacks=[CommandHandler('stop', stop)]
-    )
-
-    application.add_handler(conv_handler)
     # Запускаем приложение.
     application.run_polling()
 
