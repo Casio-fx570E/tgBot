c83ad1b48f407c10226dcf6622b40e57992d0526
CompVlad <tvr2007@mail.ru>
2023-04-17 20:01:34 +0300

    исход потраченого времени и 10 рублей за аккаунт

--- Сonfig.py
+++ Сonfig.py
@@ -7,3 +7,9 @@ VK_TOKEN = 'vk1.a.kA9w78AT9bqTCU1fD6JRgLmfVqn2uQ2EJ64oYzvKhHVLEHUlyzyiovqWBjF_57
 # fullname = user[0]['first_name'] +  ' ' + user[0]['last_name']
 # imageLink = user[0]['photo_max_orig']
 # Наработки на получение фото профиля
+VK_DATA = '+919491423606:9491423606:vk1.a.SF4wTEz-AqGzsu-777vN3YeU-wH5551MWZ97fZs0MhyQhB58C8atVy' \
+          '1bW9Jce3cguJi6eQ-J6Hq08' \
+          '-HScBeIeK8PvnxaFV_LVXcvtLKzQApaN8lKqJENAac1oB1tRDibk_vU8qhD9_LSZyXBnv3tR90-Ubc_hhaHNcT' \
+          'sd231AE1f1LADLEFa8VpLo' \
+          '_2aAWkQAVTOX-IaeD3iC9LYXZ3LJw&expires_in=86400'
+# для чего я купил этот акк(((((((((((
