b47050ff2a3a1f3c13600ade6884ce34ab479954
CompVlad <tvr2007@mail.ru>
2023-04-17 18:00:50 +0300

    Кусок регистрации через вк айди

--- Сonfig.py
+++ Сonfig.py
@@ -1,7 +1,6 @@
 TOKEN = '6287597539:AAGLJJbT5l6KkMxlGoJo1BRQmS1AQKZLfU0'
-VK_TOKEN = 'vk1.a.kA9w78AT9bqTCU1fD6JRgLmfVqn2uQ2EJ64oYzvKhHVLEHUlyzyiovqWBjF_578XAd' \
-           'X6jgUvcoAwPdA0RuCcvIsZofz9be6-6ppp0IGC8C_VCHCxymlMv3ZfueaWZBuc0PFmS8qigqjyfjvove3DxYCoXB_av8' \
-           'KXzBY2nBrhF4Aqqb0dUsdv-Ov15gf5hsCD6gGRlg9fbpsTFMYkioVsew'
+# нужен левый акк чтоб токен был(возможен взлом вашей основы)
+VK_TOKEN = ''
 # user = vk.method("users.get", {"user_ids": 1, "fields":["photo_max_orig"]})
 # fullname = user[0]['first_name'] +  ' ' + user[0]['last_name']
 # imageLink = user[0]['photo_max_orig']
